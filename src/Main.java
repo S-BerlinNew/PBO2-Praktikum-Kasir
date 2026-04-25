5feacd0e42a5d60ec464ffc5cf742d64125e84ae
Jonathanbirila <jonathanbirila9@gmail.com>
2026-04-25 16:08:50 +0700

    fix conflict

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import ui.LoginForm;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new LoginForm().setVisible(true);
-        });
-    }
-}
